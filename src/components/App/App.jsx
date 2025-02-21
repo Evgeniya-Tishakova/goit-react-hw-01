6aaa9cd22b354bf34135e7b06a2abcc430231fe3
Zhenya Tishakova <tishakovaevgeniya@gmail.com>
2025-02-21 17:11:31 +0200

    change 2 task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import Profile from "../Profile/Profile.jsx";
 import TransactionHistory from "../Transaction/TransactionHistory.jsx";
-import FriendList from "../Friend/FriendList.jsx";
+import FriendList from "../FreindList/FriendList.jsx";
 import userData from "../userData.json";
 import friends from "../friends.json";
 import transactions from "../transactions.json";
